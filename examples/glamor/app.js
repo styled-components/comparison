7dcea7c12ceb10ed2adec1a58968e0130503f8b0
Sunil Pai <threepointone@gmail.com>
2016-08-25 23:47:32 +0530

    first cut, glamor - css-components/comparison#11

---
+++ examples/glamor/app.js
@@ -0,0 +1,67 @@
+import { createElement } from 'glamor/react' // eslint-disable-line no-unused-vars
+/** @jsx createElement */
+
+import React from 'react'
+
+import { insertRule, merge, media } from 'glamor'
+import 'glamor/reset'
+
+import { vars } from 'glamor/react'
+
+import { Tweet } from './tweet'
+import data from './data.json'
+
+let variables = {
+  accent: '#1da1f2',
+  animation: '#e81c4f',
+  border: '#e1e8ed',
+  primary: '#292f33',
+  secondary: '#8899a6'
+}
+
+insertRule(`html {
+  color: ${variables.primary};
+  font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+  font-size: 14px;
+  line-height: 1.3125;
+}`)
+
+insertRule(`a {
+  text-decoration: none;
+}`)
+
+insertRule(`svg {
+  fill: currentColor;
+  height: 1.25em;
+}`)
+
+insertRule(`@media screen and (min-width: 360px) {
+  html {
+    font-size: 15px;
+  }
+}`)
+
+insertRule(`@media screen and (min-width: 600px) {
+  html {
+    font-size: 16px;
+  }
+}`)
+
+let container = merge({
+  margin: '0 auto',
+  width: '100%'
+}, media('screen and (min-width: 360px)', {
+  maxWidth: '400px'
+}), media('screen and (min-width: 600px)', {
+  maxWidth: '600px'
+}))
+
+
+@vars(variables)
+export class App extends React.Component {
+  render() {
+    return <div css={container}>
+      <Tweet data={data} />
+    </div>  
+  }
+}
