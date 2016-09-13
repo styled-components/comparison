60ccf94b5e43031e53b3d6c669513c2fde564802
Max <contact@mstoiber.com>
2016-09-13 19:55:21 +0200

    containers/app -> components/app

--- examples/css-modules/src/index.js
+++ examples/css-modules/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-import App from 'containers/app';
+import App from 'components/app';
 
 render(<App />, document.getElementById('example-root'));
