be50305bb9518c4e4984119043d2e005d798aae4
Max <contact@mstoiber.com>
2016-10-07 13:27:35 +0200

    Radium supports global styles!

--- examples/radium/src/components/app/app.js
+++ examples/radium/src/components/app/app.js
@@ -1,11 +1,37 @@
 import React from 'react';
-import radium from 'radium';
+import radium, { Style } from 'radium';
 
 import Tweet from '../tweet';
 import data from '../../../../shared/data/755481795206971392.json';
 
 import 'normalize.css';
-import '../../global.css';
+
+const globalStyles = {
+  html: {
+    color: '#292f33',
+    fontFamily: '"Helvetica Neue", Helvetica, Arial, sans-serif',
+    fontSize: 14,
+    lineHeight: 1.3125,
+  },
+  a: {
+    textDecoration: 'none',
+    color: '#1da1f2',
+  },
+  svg: {
+    fill: 'currentColor',
+    height: '1.25em',
+  },
+  '@media screen and (min-width: 360px)': {
+    html: {
+      fontSize: 15,
+    },
+  },
+  '@media screen and (min-width: 600px)': {
+    html: {
+      fontSize: 16,
+    },
+  },
+};
 
 const styles = {
   container: {
@@ -22,6 +48,7 @@ const styles = {
 
 const App = () => (
   <div style={styles.container}>
+    <Style rules={globalStyles} />
     <Tweet data={data} />
   </div>
 );
